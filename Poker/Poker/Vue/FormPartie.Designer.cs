a1bf0c066a1bcc5b6750fcc7ed9bcd541c773be0
Laureneb27 <bidaux.laurene@gmail.com>
2018-12-28 08:31:46 +0100

    Boutton Refresh view

--- Poker/Poker/Vue/FormPartie.Designer.cs
+++ Poker/Poker/Vue/FormPartie.Designer.cs
@@ -44,6 +44,7 @@
             this.pictureBox5 = new System.Windows.Forms.PictureBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.btn_refresh = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.table)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_mise)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
@@ -58,26 +59,29 @@
             this.table.BackColor = System.Drawing.Color.Transparent;
             this.table.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("table.BackgroundImage")));
             this.table.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.table.Location = new System.Drawing.Point(-64, -102);
+            this.table.Location = new System.Drawing.Point(-85, -126);
+            this.table.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.table.Name = "table";
-            this.table.Size = new System.Drawing.Size(1366, 768);
+            this.table.Size = new System.Drawing.Size(1821, 945);
             this.table.TabIndex = 7;
             this.table.TabStop = false;
             // 
             // button_seCoucher
             // 
-            this.button_seCoucher.Location = new System.Drawing.Point(589, 540);
+            this.button_seCoucher.Location = new System.Drawing.Point(785, 665);
+            this.button_seCoucher.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button_seCoucher.Name = "button_seCoucher";
-            this.button_seCoucher.Size = new System.Drawing.Size(75, 23);
+            this.button_seCoucher.Size = new System.Drawing.Size(100, 28);
             this.button_seCoucher.TabIndex = 8;
             this.button_seCoucher.Text = "Se coucher";
             this.button_seCoucher.UseVisualStyleBackColor = true;
             // 
             // button_miser
             // 
-            this.button_miser.Location = new System.Drawing.Point(1059, 540);
+            this.button_miser.Location = new System.Drawing.Point(1412, 665);
+            this.button_miser.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button_miser.Name = "button_miser";
-            this.button_miser.Size = new System.Drawing.Size(75, 23);
+            this.button_miser.Size = new System.Drawing.Size(100, 28);
             this.button_miser.TabIndex = 9;
             this.button_miser.Text = "Miser";
             this.button_miser.UseVisualStyleBackColor = true;
@@ -85,18 +89,20 @@
             // 
             // button_check
             // 
-            this.button_check.Location = new System.Drawing.Point(796, 540);
+            this.button_check.Location = new System.Drawing.Point(1061, 665);
+            this.button_check.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button_check.Name = "button_check";
-            this.button_check.Size = new System.Drawing.Size(75, 23);
+            this.button_check.Size = new System.Drawing.Size(100, 28);
             this.button_check.TabIndex = 10;
             this.button_check.Text = "Check";
             this.button_check.UseVisualStyleBackColor = true;
             // 
             // button_suivre
             // 
-            this.button_suivre.Location = new System.Drawing.Point(683, 540);
+            this.button_suivre.Location = new System.Drawing.Point(911, 665);
+            this.button_suivre.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button_suivre.Name = "button_suivre";
-            this.button_suivre.Size = new System.Drawing.Size(75, 23);
+            this.button_suivre.Size = new System.Drawing.Size(100, 28);
             this.button_suivre.TabIndex = 11;
             this.button_suivre.Text = "Suivre";
             this.button_suivre.UseVisualStyleBackColor = true;
@@ -104,14 +110,15 @@
             // 
             // numericUpDown_mise
             // 
-            this.numericUpDown_mise.Location = new System.Drawing.Point(912, 543);
+            this.numericUpDown_mise.Location = new System.Drawing.Point(1216, 668);
+            this.numericUpDown_mise.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.numericUpDown_mise.Maximum = new decimal(new int[] {
             1410065408,
             2,
             0,
             0});
             this.numericUpDown_mise.Name = "numericUpDown_mise";
-            this.numericUpDown_mise.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown_mise.Size = new System.Drawing.Size(160, 22);
             this.numericUpDown_mise.TabIndex = 12;
             this.numericUpDown_mise.ValueChanged += new System.EventHandler(this.numericUpDown_mise_ValueChanged);
             // 
@@ -119,9 +126,10 @@
             // 
             this.label_pseudo.AutoSize = true;
             this.label_pseudo.BackColor = System.Drawing.Color.Salmon;
-            this.label_pseudo.Location = new System.Drawing.Point(142, 497);
+            this.label_pseudo.Location = new System.Drawing.Point(189, 612);
+            this.label_pseudo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label_pseudo.Name = "label_pseudo";
-            this.label_pseudo.Size = new System.Drawing.Size(43, 13);
+            this.label_pseudo.Size = new System.Drawing.Size(56, 17);
             this.label_pseudo.TabIndex = 13;
             this.label_pseudo.Text = "Pseudo";
             // 
@@ -129,54 +137,60 @@
             // 
             this.label_argent.AutoSize = true;
             this.label_argent.BackColor = System.Drawing.Color.Salmon;
-            this.label_argent.Location = new System.Drawing.Point(147, 520);
+            this.label_argent.Location = new System.Drawing.Point(196, 640);
+            this.label_argent.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label_argent.Name = "label_argent";
-            this.label_argent.Size = new System.Drawing.Size(38, 13);
+            this.label_argent.Size = new System.Drawing.Size(50, 17);
             this.label_argent.TabIndex = 14;
             this.label_argent.Text = "Argent";
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(1021, 44);
+            this.pictureBox1.Location = new System.Drawing.Point(1361, 54);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(86, 119);
+            this.pictureBox1.Size = new System.Drawing.Size(115, 146);
             this.pictureBox1.TabIndex = 15;
             this.pictureBox1.TabStop = false;
             // 
             // pictureBox2
             // 
             this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(1053, 44);
+            this.pictureBox2.Location = new System.Drawing.Point(1404, 54);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(86, 119);
+            this.pictureBox2.Size = new System.Drawing.Size(115, 146);
             this.pictureBox2.TabIndex = 16;
             this.pictureBox2.TabStop = false;
             // 
             // pictureBox3
             // 
             this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
-            this.pictureBox3.Location = new System.Drawing.Point(408, 207);
+            this.pictureBox3.Location = new System.Drawing.Point(544, 255);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(86, 119);
+            this.pictureBox3.Size = new System.Drawing.Size(115, 146);
             this.pictureBox3.TabIndex = 17;
             this.pictureBox3.TabStop = false;
             // 
             // pictureBox4
             // 
             this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
-            this.pictureBox4.Location = new System.Drawing.Point(499, 207);
+            this.pictureBox4.Location = new System.Drawing.Point(665, 255);
+            this.pictureBox4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(86, 119);
+            this.pictureBox4.Size = new System.Drawing.Size(115, 146);
             this.pictureBox4.TabIndex = 18;
             this.pictureBox4.TabStop = false;
             // 
             // pictureBox5
             // 
             this.pictureBox5.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox5.Image")));
-            this.pictureBox5.Location = new System.Drawing.Point(590, 207);
+            this.pictureBox5.Location = new System.Drawing.Point(787, 255);
+            this.pictureBox5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox5.Name = "pictureBox5";
-            this.pictureBox5.Size = new System.Drawing.Size(86, 119);
+            this.pictureBox5.Size = new System.Drawing.Size(115, 146);
             this.pictureBox5.TabIndex = 19;
             this.pictureBox5.TabStop = false;
             // 
@@ -184,9 +198,10 @@
             // 
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.Salmon;
-            this.label1.Location = new System.Drawing.Point(1069, 217);
+            this.label1.Location = new System.Drawing.Point(1425, 267);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.Size = new System.Drawing.Size(50, 17);
             this.label1.TabIndex = 21;
             this.label1.Text = "Argent";
             // 
@@ -194,17 +209,29 @@
             // 
             this.label2.AutoSize = true;
             this.label2.BackColor = System.Drawing.Color.Salmon;
-            this.label2.Location = new System.Drawing.Point(1064, 194);
+            this.label2.Location = new System.Drawing.Point(1419, 239);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.Size = new System.Drawing.Size(56, 17);
             this.label2.TabIndex = 20;
             this.label2.Text = "Pseudo";
             // 
+            // btn_refresh
+            // 
+            this.btn_refresh.Location = new System.Drawing.Point(64, 39);
+            this.btn_refresh.Name = "btn_refresh";
+            this.btn_refresh.Size = new System.Drawing.Size(75, 23);
+            this.btn_refresh.TabIndex = 22;
+            this.btn_refresh.Text = "Refresh";
+            this.btn_refresh.UseVisualStyleBackColor = true;
+            
+            // 
             // FormPartie
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1228, 657);
+            this.ClientSize = new System.Drawing.Size(1637, 809);
+            this.Controls.Add(this.btn_refresh);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.pictureBox5);
@@ -220,7 +247,7 @@
             this.Controls.Add(this.button_miser);
             this.Controls.Add(this.button_seCoucher);
             this.Controls.Add(this.table);
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "FormPartie";
             this.Text = "FormPartie";
             ((System.ComponentModel.ISupportInitialize)(this.table)).EndInit();
@@ -252,6 +279,7 @@
         private System.Windows.Forms.PictureBox pictureBox5;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn_refresh;
     }
 }
 
--- Poker/Poker/Vue/FormPartie.cs
+++ Poker/Poker/Vue/FormPartie.cs
@@ -34,5 +34,7 @@ namespace Poker
         {
             
         }
+
+       
     }
 }
